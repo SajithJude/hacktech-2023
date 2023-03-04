267e1fb9d62787d6b7a42900f1b29d787b51642b
Jude Sajith <65233121+SajithJude@users.noreply.github.com>
2023-03-04 18:29:17 +0000

    sss

--- app.py
+++ app.py
@@ -1,3 +1,32 @@
 import streamlit as st
+import pandas as pd
+import altair as alt
 
-st.write("Hello world")
+# Set page title
+st.set_page_config(page_title='CSV Scatter Plot', layout='wide')
+
+# Page title and description
+st.title('CSV Scatter Plot')
+st.write('Upload a CSV file, select columns to plot, and generate a scatter plot.')
+
+# Upload file
+uploaded_file = st.file_uploader('Upload CSV', type=['csv'])
+
+if uploaded_file is not None:
+    # Read CSV
+    df = pd.read_csv(uploaded_file)
+
+    # Select columns to plot
+    st.write('### Select Columns to Plot')
+    columns = list(df.columns)
+    x_col = st.selectbox('X axis', options=columns)
+    y_col = st.selectbox('Y axis', options=columns)
+
+    # Generate scatter plot
+    st.write('### Scatter Plot')
+    chart = alt.Chart(df).mark_circle().encode(
+        x=x_col,
+        y=y_col,
+        tooltip=columns
+    ).interactive()
+    st.altair_chart(chart, use_container_width=True)
