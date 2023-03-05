f6c906b6abb4e07675f687615aff3371f59090ee
Jude Sajith <65233121+SajithJude@users.noreply.github.com>
2023-03-05 16:10:36 +0000

    sss

--- app.py
+++ app.py
@@ -11,7 +11,14 @@ st.set_page_config(page_title='CodeGen', layout='wide')
 # Page title and description
 st.title('CodeGen')
 st.write('A tool By the community for the community to make sustainable innovating easy...')
-
+st.markdown("""
+    
+#### Click on the Arrow icon on the top left corner to trigger the menu , 
+#### The option "Explanation Training code" will explain any uploaded dataset and generate a code and guide you to train a machine learning model in a few button clicks,
+#### The Option "Upload dataset for Equations & Simulations" will allow you to upload a dataset for the next option to work
+#### The Final Option "Equations and Simulations would generate mathematical equations from the interaction between any selected columns and generate code to perform a stimulation the scenario
+#### If you want to visualize Columns on your own to find relationships, expand the option bellow
+""")
 # Upload file
 
 with st.expander("Visualization tool"):
